16015c71369daec6f8c3cc94486d2d1b95b9ae95
atiffany <78195948+amytiffanyloo@users.noreply.github.com>
2022-12-10 02:52:40 -0500

    Add Class and Songs
    - added a class with display and change functions
    - random song generation

---
+++ class.js
@@ -0,0 +1,24 @@
+    class Breath{
+    constructor(){
+    this.x = random(100,1200);
+    this.y = random(80,650);
+    this.size = 0;
+    this.r = random(100,200);
+    this.g =random(100,200);
+    this.b = random(100,200);
+    this.alpa = 0;
+    }
+
+display(){
+    fill(this.r,this.g,this.b,this.alpha);
+    ellipse(this.x,this.y,this.size);
+}
+
+change(){
+  let level = amp.getLevel();
+  let ellipseColor = round(map(level, 0, 1, 0, 15000));
+    this.alpha = ellipseColor;
+     let ellipseSize = round(map(level,0,1,0,100));
+  this.size = random(50,300)*ellipseSize; 
+}
+}
--- script1.js
+++ script1.js
@@ -1,81 +1,71 @@
 let mic,amp;
 let song1, song2, song3;
-let songs = []
-let breaths =[]
+let song;
+let songs = [];
+let breaths1 =[];
+let breaths2 =[];
+let mode = 255;
+let songChoice;
 
 function preload(){
   soundFormats('mp3')
-  song1 = loadSound('song1.mp3');
-//   song2 = loadSound('song2.mp3');
-//   song3 = loadSound('song2.mp3');
+  songs[0] = loadSound('song1.mp3');
+  songs[1] = loadSound('song2.mp3');
+  song[2] = loadSound('song2.mp3');
   console.log("song all loaded");
 }
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1200, 1000);
   background(255);
-  frameRate(5);
-//   songs =[song1, song2, song3]
+  songChoice = floor(random(0,3));
+  song = songs[songChoice];
+  frameRate(3);
+    noStroke()
   amp = new p5.Amplitude();
-  amp.setInput(song1);
-//   for (i=0; i<5;i++){
-//     breaths[i] = new Breath(100,200,200);
-// }
+  amp.setInput(song);
+  for (i=0; i<4;i++){
+    breaths1[i] = new Breath(random(100,300));
 }
-
-// class Breath{
-//     constructor(r,g,b){
-//     this.x = random(width);
-//     this.y = random(height);
-//     this.size = random(100,400);
-//     this.r = r;
-//     this.g = g;
-//     this.b = b;
-//     }
-
-// display(){
-//     fill(this.r,this.b,this.g);
-//     ellipse(this.x,this.y,this.size);
-// }
-
-// change(){
-//     this.r = ellipseColor*0.5
-//     this.b = ellipseColor*0.5
-// }
-
-// }
+  for (i=0; i<3;i++){
+    breaths2[i] = new Breath(random(100,300));
+}
+}
 
 function mousePressed(){
-  if (song1.isPlaying()) {
+  if (song.isPlaying()) {
     // .isPlaying() returns a boolean
-    song1.stop();
+    song.stop();
   } else {
-    song1.play(0,1,0.5,1,300);
+    song.play(0,1,0.5,1,300);
   }
-}
-
-function art(){
- let level = amp.getLevel();
-  let ellipseColor = map(level, 0, 1, 0, 15000);
-  let ellipseSize = map(level,0,1,0,20000)
-  text(ellipseColor,100,400);
-  noStroke()
-  push();
-  fill(ellipseColor*0.5,ellipseColor,ellipseColor*0.5,ellipseColor*0.5)
-  ellipse(200,200,ellipseSize*0.5)
-  fill(200,ellipseColor*0.5,ellipseColor*0.5,ellipseColor*0.5)
-  ellipse(280,200,ellipseSize*0.3)
-  pop();
-  console.log(ellipseColor)    
 }
 
 function draw() {
     background(255);
-    text("Click anywhere to begin your meditation session",100,200);
-    art();
-    //   console.log("this is song duration:" + song1.duration());
-    //   for (i=0;i<breaths.length;i++){
-    //     breaths[i].display();
-    //     breaths[i].change();
-    //   }
+      for (i=0;i<breaths1.length;i++){
+        breaths1[i].display();
+        breaths1[i].change();
+      }
+        for (i=0;i<breaths2.length;i++){
+        breaths2[i].display();
+        breaths2[i].change();
+      }
+          let level = amp.getLevel();
+  let ellipseColor = round(map(level, 0, 1, 0, 15000));
+  let ellipseSize = round(map(level,0,1,0,20000));
+  push();
+    fill(mode);
+  text("Ellipse Size: " + ellipseSize,5,10);
+  text("Ellipse Color: " + ellipseColor,5,20);
+  pop();
     }
+
+//Turning on debug mode by changing alpha of text color
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    mode = 0;
+  } else if (keyCode === RIGHT_ARROW) {
+    mode = 255;
+  }
+}
